558b7f2e354ba42195f260be73e34bcf671c678a
aashna neupane <aashna833@gmail.com>
2024-04-08 21:36:28 +0545

    added cutscene

---
+++ Assets/Scripts/cutScene.cs
@@ -0,0 +1,22 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutScene : MonoBehaviour
+{
+    public GameObject cutsceneCam, player;
+    public float cutsceneTime;
+
+    void Start()
+    {
+        StartCoroutine(cutscene());
+    }
+    IEnumerator cutscene()
+    {
+        yield return new WaitForSeconds(cutsceneTime);
+        player.SetActive(true);
+        cutsceneCam.SetActive(false);
+    }
+}
+
